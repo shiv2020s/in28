25df5115c80094987a1ca1c93dcf45eef63b0987
shiv2020s <kavi20900@gmail.com>
2020-05-16 14:55:21 +0530

    third commit

--- src/main/java/com/example/demo/Hellocontroller.java
+++ src/main/java/com/example/demo/Hellocontroller.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class Hellocontroller {
@@ -7,6 +8,10 @@ public class Hellocontroller {
 	@RequestMapping(value="timezone")
 	public String timezone() {
 		return "time";
+	}
+	@GetMapping()
+	public String name() {
+		return "index.html";
 	}
 	
 	
